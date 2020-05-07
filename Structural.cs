f74c46483ca1085ead757af16444182109520d70
rafaelxoliohub <rafa.x.reyes.dev@hotmail.com>
2020-05-07 17:42:10 -0500

    Add project files.

---
+++ Structural.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPattern.Singleton
+{
+    public static class Structural
+    {
+        public static void Run() {
+            Singleton instance1 = Singleton.GetInstance();
+            Singleton instance2 = Singleton.GetInstance();
+
+            if (instance1 == instance2)
+            {
+                Console.WriteLine("Same Singleton Object");
+            }
+
+            Console.ReadKey();
+        }
+    }
+
+    class Singleton
+    {
+        private static Singleton instance;
+
+        protected Singleton() { 
+        }
+
+        public static Singleton GetInstance()
+        {
+            if (instance==null)
+            {
+                instance = new Singleton();
+            }
+
+            return instance;
+        }
+    }
+}
